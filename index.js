2f3fa1d45309fa6117a17f35dc38c7c38bf8ff93
inventor2007 <66035084+inventor2007@users.noreply.github.com>
2024-02-08 19:49:28 +0100

    Update

--- index.js
+++ index.js
@@ -1,38 +1,154 @@
-// Example Nfc Access Plugin
+let Service, Characteristic
+const packageJson = require('./package.json')
+const request = require('request')
 
-module.exports = (api) => {
-  api.registerAccessory('nfc-card', ExampleNFCAccessAccessory);
-};
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-http-nfc-lock', 'HTTPLock', HTTPLock)
+}
 
-class ExampleNFCAccessAccessory {
+function HTTPLock (log, config) {
+  this.log = log
 
-  constructor(log, config, api) {
-    this.log = log;
-    this.config = config;
-    this.api = api;
+  this.name = config.name
+  this.apiroute = config.apiroute
 
-    this.Service = this.api.hap.Service;
-    this.Characteristic = this.api.hap.Characteristic;
-    console.log(this.Characteristic);
+  this.autoLock = config.autoLock || false
+  this.autoLockDelay = config.autoLockDelay || 10
 
-    // extract name from config
-    this.name = config.name;
+  this.manufacturer = config.manufacturer || packageJson.author
+  this.serial = config.serial || this.apiroute
+  this.model = config.model || packageJson.name
+  this.firmware = config.firmware || packageJson.version
 
-    // create a new Nfc Access service
-    this.service = new this.Service.NFCAccess(this.name);
+  this.username = config.username || null
+  this.password = config.password || null
+  this.timeout = config.timeout || 5000
+  this.http_method = config.http_method || 'GET'
 
-    this.service.setCharacteristic(Characteristic.NFCAccessSupportedConfiguration, 1);
+  this.pollInterval = config.pollInterval || 120
 
-    // create handlers for required characteristics
-    this.service.getCharacteristic(this.Characteristic.ConfigurationState)
-      .onGet(console.log(this));
+  if (this.username != null && this.password != null) {
+    this.auth = {
+      user: this.username,
+      pass: this.password
+    }
+  }
 
-    this.service.getCharacteristic(this.Characteristic.NFCAccessControlPoint)
-      .onGet(console.log(this))
-      .onSet(console.log(this));
+  this.lockMechanismService = new Service.LockMechanism(this.name)
+  this.lockManagementService = new Service.LockManagement(this.name)
 
-    this.service.getCharacteristic(this.Characteristic.NFCAccessSupportedConfiguration)
-      .onGet(console.log(this));
 
+  this.nfcAccessService = new Service.NFCAccess(this.name)
+  this.nfcAccessService.setCharacteristic(Characteristic.NFCAccessSupportedConfiguration, "AQEQAgEQ");
+}
+
+HTTPLock.prototype = {
+
+  identify: function (callback) {
+    this.log('Identify requested!')
+    callback()
+  },
+
+  _httpRequest: function (url, body, method, callback) {
+    request({
+      url: url,
+      body: body,
+      method: this.http_method,
+      timeout: this.timeout,
+      rejectUnauthorized: false,
+      auth: this.auth
+    },
+    function (error, response, body) {
+      callback(error, response, body)
+    })
+  },
+
+  _getStatus: function (callback) {
+    const url = this.apiroute + '/status'
+    this.log.debug('Getting status: %s', url)
+
+    this._httpRequest(url, '', 'GET', function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error getting status: %s', error.message)
+        this.lockMechanismService.getCharacteristic(Characteristic.LockCurrentState).updateValue(new Error('Polling failed'))
+        callback(error)
+      } else {
+        this.log.debug('Device response: %s', responseBody)
+        try {
+          const json = JSON.parse(responseBody)
+          this.lockMechanismService.getCharacteristic(Characteristic.LockCurrentState).updateValue(json.currentState)
+          this.lockMechanismService.getCharacteristic(Characteristic.LockTargetState).updateValue(json.currentState)
+          this.log.debug('Updated state to: %s', json.currentState)
+          callback()
+        } catch (e) {
+          this.log.warn('Error parsing status: %s', e.message)
+        }
+      }
+    }.bind(this))
+  },
+
+  setLockTargetState: function (value, callback) {
+    const url = this.apiroute + '/setState?value=' + value
+    this.log.debug('Setting state: %s', url)
+
+    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
+      if (error) {
+        this.log.warn('Error setting state: %s', error.message)
+        callback(error)
+      } else {
+        this.log('Set state to %s', value)
+        this.lockMechanismService.getCharacteristic(Characteristic.LockCurrentState).updateValue(value)
+        if (value === 1 && this.autoLock) {
+          this.autoLockFunction()
+        }
+        callback()
+      }
+    }.bind(this))
+  },
+
+  autoLockFunction: function () {
+    this.log('Waiting %s seconds for autolock', this.autoLockDelay)
+    setTimeout(() => {
+      this.lockMechanismService.setCharacteristic(Characteristic.LockTargetState, 1)
+      this.log('Autolocking...')
+    }, this.autoLockDelay * 1000)
+  },
+
+  getServices: function () {
+    this.informationService = new Service.AccessoryInformation()
+    this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+      .setCharacteristic(Characteristic.Model, this.model)
+      .setCharacteristic(Characteristic.SerialNumber, this.serial)
+      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
+
+    this.lockMechanismService
+      .getCharacteristic(Characteristic.LockTargetState)
+      .on('set', this.setLockTargetState.bind(this))
+
+    this.nfcAccessService.setCharacteristic(Characteristic.NFCAccessSupportedConfiguration, 1);
+
+    this.nfcAccessService
+        .getCharacteristic(Characteristic.ConfigurationState)
+        .on(CharacteristicEventTypes.GET, callback => {
+          console.log("Queried config state ");
+          callback(undefined, 0);
+        });
+    this.nfcAccessService
+        .getCharacteristic(Characteristic.NFCAccessControlPoint)
+        .on(CharacteristicEventTypes.SET, (value, callback) => {
+          console.log("Control Point Write: " + value);
+          callback(undefined, "");
+        });
+
+    this._getStatus(function () {})
+
+    setInterval(function () {
+      this._getStatus(function () {})
+    }.bind(this), this.pollInterval * 1000)
+
+    return [this.informationService, this.lockMechanismService, this.lockManagementService, this.lockMechanismService, this.nfcAccessService]
   }
 }
